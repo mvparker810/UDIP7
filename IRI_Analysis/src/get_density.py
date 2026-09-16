f4ac447d022520902d8eb939832d6d0577c00cce
Michael <mwomer27@gmail.com>
2026-09-16 16:24:30 -0400

    skeleton script for getting electron density, needs more specifics

---
+++ IRI_Analysis/src/get_density.py
@@ -0,0 +1,33 @@
+import numpy as np
+import PyIRI.sh_library as sh
+
+#coordinates for wallops
+LAT = 37.9402
+LON = -75.4664
+
+YEAR, MONTH, DAY = 2026, 5, 15
+
+LOCAL_HOUR = 5.0
+UTC_OFFSET_HOURS = -4.0   # EDT = UTC-4
+UT_HOUR = LOCAL_HOUR - UTC_OFFSET_HOURS
+
+
+F107 = 150 #seems like average, will investigate further
+
+
+ALT_MIN = 0 #minimum altitude
+ALT_MAX = 200 #max altitude
+ALT_STEP = 1 #"fidelity"
+
+aalt = np.arange(ALT_MIN, ALT_MAX, ALT_STEP)
+
+*_, density_profile = sh.IRI_density_1day( #*_ to absorb uneeded return values, all we need right now is denstiy profile
+    YEAR, MONTH, DAY, UT_HOUR, LON, LAT, aalt, F107,
+    coeff_dir=None,
+    foF2_coeff='URSI',
+    hmF2_model='SHU2015',
+    coord='GEO',
+    old_output=False)
+
+print(density_profile)
+
